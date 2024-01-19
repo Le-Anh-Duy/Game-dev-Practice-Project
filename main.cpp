0966e7dc4c3fb1b7f63be5a9dd49ec762cac6cd8
Duy Le <duyla25clc@gmail.com>
2024-01-19 12:27:31 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[]) 
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = 
+    SDL_CreateWindow(
+        "Hello world", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        WIDTH, HEIGHT, 
+        SDL_WINDOW_ALLOW_HIGHDPI
+    );
+
+    if (NULL == window) {
+        std::cout << "Could not create window" << SDL_GetError() << '\n';
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+
+    while (true) {
+        if (SDL_PollEvent( &windowEvent )) {
+            if (SDL_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+    std::cout << "Quit succeeded";
+
+    return EXIT_SUCCESS;
+}
